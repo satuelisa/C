21651d3b4d0f08feb833b99d2fd49ab51fa60626
Elisa Schaeffer <elisa.schaeffer@gmail.com>
2021-02-04 15:45:40 -0600

    While loop demo

--- test.c
+++ test.c
@@ -4,7 +4,7 @@ int main(int argc, char** args) { // we will talk about this stuff later
   int x, y, z; // integers
 
   x = 13; // define initial values
-  y = 29;
+  y = 7;
 
   z = x; // swap values
   x = y;
@@ -13,5 +13,11 @@ int main(int argc, char** args) { // we will talk about this stuff later
   if (x > y) {
     printf("hello\n");
   }
+
+  while (x > 0) { // loop (cycle)
+    printf("%i\n", 2 * x); // print an integer
+    x--; // lower by one
+  }
+  
   return 1;
 }
