b273a21ec82b9ba59bdbfad5a08951116efc9ea5
Elisa Schaeffer <elisa.schaeffer@gmail.com>
2021-02-04 15:40:17 -0600

    More demos

--- test.c
+++ test.c
@@ -4,7 +4,7 @@ int main(int argc, char** args) { // we will talk about this stuff later
   int x, y, z; // integers
 
   x = 13; // define initial values
-  y = 9;
+  y = 29;
 
   z = x; // swap values
   x = y;
