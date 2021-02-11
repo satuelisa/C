55cac977e84f38906ef97d95e56857771725f4f1
Elisa Schaeffer <elisa.schaeffer@gmail.com>
2021-02-11 15:05:01 -0600

    Added an atoi example

--- test.c
+++ test.c
@@ -1,17 +1,45 @@
-#include <stdio.h> // standard input and output
+#include <stdio.h> // printf
+#include <stdlib.h> // atoi
 
-int main(int argc, char** args) { // we will talk about this stuff later
-  int x, y, z; // integers
+/*
+ return type = integer = numero entero
+ argc = how many command-line arguments do we have
+ args = pointer to an ARRAY of the command-line arguments as character arrays
+*/
+int main(int loque, char** sea) { 
+  int x, y, z; /* integer variables (storage space) */
 
-  x = 3; // define initial values
-  y = 7;
+  for (x = 0; x < loque; x++ ) {
+    printf("%i: %s\n", x, sea[x]);
+  }
+  printf("3rd char of 1st string: %c\n", sea[0][2]);  
+
+  x = atoi(sea[1]); // take an %s and make it into an %i
+  y = atoi(sea[2]);
+  z = atoi(sea[3]);
+  printf("x is %i and y is %i and x * y is %i\n", x, y, x * y);
+  printf("z is %i and x * z is %i\n", z, x * z);  
 
   z = x; // swap values
   x = y;
   y = z;
 
-  if (x > y) {
-    printf("if: hello\n");
+  if (x > y) { // condition
+    printf("1st if: hello\n");
+  }
+
+  if (x < y) {
+    printf("2nd if: first branch\n");
+  } else {
+    printf("2nd if: second branch\n");
+  }
+
+  if (x < y) {
+    printf("3rd if: first branch\n");
+  } else if (x < 2 * y) {
+    printf("3rd if: second branch\n");
+  } else {
+    printf("3rd if: final branch\n");
   }
 
   z = x;
