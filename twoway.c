a7836cd7b60f7dfeff433383a19e7735464be492
Elisa Schaeffer <elisa.schaeffer@gmail.com>
2021-02-18 15:25:57 -0600

    Making it complicated

--- twoway.c
+++ twoway.c
@@ -5,15 +5,17 @@
 #define STEP 40
 
 int main () {
-  int t, direction = 1;
+  int t, dir = 1, ord = 0; // flag
 
-  if (direction == 0) {
+  if (dir == 0) {
     printf("F\tC\n--------------\n");
   } else {
     printf("C\tF\n--------------\n");
   }
-  for (t = LOWER; t <= UPPER; t += STEP) {
-    printf("%3d\t%3d\n", t, (int)round(direction == 0 ? 5 * (t - 32) / 9 : 9 * t / 5 + 32));
+  for (t = (ord == 1 ? LOWER : UPPER);
+       (t <= UPPER && ord == 1) || (t >= LOWER && ord != 1);
+       t += STEP * (ord == 1 ? 1 : -1)) {
+    printf("%3d\t%3d\n", t, (int)round(dir == 0 ? 5 * (t - 32) / 9 : 9 * t / 5 + 32));
   }
   return 0;
 }
