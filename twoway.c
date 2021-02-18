8ac33711ebeadb45e49092778bf97b0ec7cde00f
Elisa Schaeffer <elisa.schaeffer@gmail.com>
2021-02-18 15:47:50 -0600

    Exercise 1-9 that is easy to convert to 1-12 as well

--- twoway.c
+++ twoway.c
@@ -5,17 +5,17 @@
 #define STEP 40
 
 int main () {
-  int t, dir = 1, ord = 0; // flag
+  int t, dir = 1, ord = 0; // flags: dir for 1-4, ord 1-5
 
-  if (dir == 0) {
-    printf("F\tC\n--------------\n");
+  if (dir == 0) { // Ex. 1-3 and 1-4
+    printf("F\tC\n--------------\n"); // print a header
   } else {
     printf("C\tF\n--------------\n");
   }
-  for (t = (ord == 1 ? LOWER : UPPER);
-       (t <= UPPER && ord == 1) || (t >= LOWER && ord != 1);
-       t += STEP * (ord == 1 ? 1 : -1)) {
-    printf("%3d\t%3d\n", t, (int)round(dir == 0 ? 5 * (t - 32) / 9 : 9 * t / 5 + 32));
+  for (t = (ord == 1 ? LOWER : UPPER); // Ex. 1-5 up or down
+       (t <= UPPER && ord == 1) || (t >= LOWER && ord != 1); // if (this and this) or (this and this)
+       t += STEP * (ord == 1 ? 1 : -1)) { // increment or decrement depending on the direction
+    printf("%3d\t%3d\n", t, (int)round(dir == 0 ? 5 * (t - 32) / 9 : 9 * t / 5 + 32)); // mini-if cond ? true : false 
   }
   return 0;
 }
