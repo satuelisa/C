5fb503e231ab9a1541b1866a6a1ce588a67c0346
Elisa Schaeffer <elisa.schaeffer@gmail.com>
2021-02-25 08:29:50 -0600

    Obsolete
    Moved these, forgot to delete

--- copycat2.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h> // para getchar y putchar y EOF
-
-int main() {
-  int c;
-  while ((c = getchar()) != EOF) { // asigna y checa directo
-    putchar(c);
-  }
-  return 1;
-}
--- examples1_1_a.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-
-int main(int x, char** y) {
-  printf("hello, world\n");
-  return 1;
-}
