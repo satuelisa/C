acc35d06e2fb6009a65ceab84195e2b2f933c1bb
Elisa Schaeffer <elisa.schaeffer@gmail.com>
2021-03-11 15:59:47 -0600

    Example starter for HW3

---
+++ Ch3/hw3.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <ctype.h>
+#define ON 1
+#define OFF 0
+
+int main() {
+  char c;
+  int s, d = OFF, p, e; // digits or punctuation
+
+  while ((c = getchar()) != EOF) {
+    s = OFF;
+    p = OFF;
+    switch (c) {
+    case ',':
+    case '.':
+    case '!':
+    case '?':
+      p = ON;
+      break;
+    case '(':
+      e = ON;
+      break;
+    case ')':
+      e = OFF;
+      break;
+    }
+    if (!e && isdigit(c)) {
+      d = ON;
+    } else {
+      if (d == ON) {
+	putchar('X');
+	s = ON;
+      }
+      d = OFF;
+    }
+    if (!p && !d && !s) {
+      putchar(c);
+    }
+  }
+  
+  return 0;
+}
