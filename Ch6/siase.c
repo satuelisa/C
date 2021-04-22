cad78d1193e0bea6f4987ecd160e8707b13de6c1
Elisa Schaeffer <elisa.schaeffer@gmail.com>
2021-04-22 15:34:18 -0500

    A second variable without using the typedef

--- Ch6/siase.c
+++ Ch6/siase.c
@@ -1,18 +1,21 @@
-#include <stdio.h>
-#include "siase.h"
+#include <stdio.h> // for printf
+#include "siase.h" // our new header
 
 int main() {
   alumno fulano;
+  struct estudiante wey;
   fulano.matr = 123456;
   fulano.pe = "ITS";
   imprime(fulano);
-  fulano.matr = 1234567;
-  fulano.pe = "IMA";
-  imprime(fulano);  
+  wey.matr = 1234567;
+  wey.pe = "IMA";
+  imprime(wey);  
   return 1;
 }
 
 void imprime(alumno a) {
+  // print the first field always with seven digits
+  // add leading zeroes to fill the digits if they are fewer
   printf("%07d es %s\n", a.matr, a.pe);
   return;
 }
