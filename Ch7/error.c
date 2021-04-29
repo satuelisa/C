4e59c38826fb49538446306460c2084f1b13dd19
Elisa Schaeffer <elisa.schaeffer@gmail.com>
2021-04-29 11:56:32 -0500

    stderr example

---
+++ Ch7/error.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define MAXLEN 9
+
+int input() {
+  int i, count;
+  char u[MAXLEN];
+  char n[MAXLEN];
+  char p[MAXLEN];
+  scanf("%d", &count);
+  if (count < 1) {
+    fprintf(stderr, "Only strictly positive counts are permitted, exiting...\n");
+    exit(1);
+  }
+  for (i = 0; i < count;) {
+    scanf("%s %s %s\n", n, p, u);
+    printf("Student %d %s (%s) is %s\n", ++i, u, n, p);
+  }
+  return count;
+}
+
+int main(int c, char** d) {
+  int count = input();
+  fprintf(stderr, "%d records processed\n", count);
+  return 0;
+}
