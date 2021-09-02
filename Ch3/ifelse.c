33e2f94141e898bee2056f2c601d0bad32beb0e0
Elisa Schaeffer <elisa.schaeffer@gmail.com>
2021-09-02 15:21:56 -0500

    Discussion of if and else and else-if

---
+++ Ch3/ifelse.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+  int i;
+  
+  if (1 < 2) {
+    for (i = 0; i < 3; i++) {
+      if (14 > 5) {
+	printf("lol\n");
+        return 0;
+      } else if (12 > 3) {
+	printf("wey\n");
+      }
+    }
+  } else {
+    printf("hola\n");
+  }
+  printf("bye\n");  
+  return 1;
+}
